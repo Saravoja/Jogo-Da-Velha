1e4ee1672d91fa8a0e2bb56416ce26bdde8c99aa
Pedro Henrique de Holanda Lousada <131923557+Saravoja@users.noreply.github.com>
2023-06-01 19:33:42 -0300

    Jogo_da_velha_Definitive_Edition.c

--- Jogo_da_velha.c
+++ Jogo_da_velha.c
@@ -3,7 +3,7 @@
 #include<ctype.h>
 #include<stdlib.h>
 #include<stdbool.h>
-
+//Grupo: Pedro Henrique de Holanda Lousada, Roger Daiki Hata Sakai, Luiz Henrique Nogueira dos Santos.
 bool win_condition(char v[3][3]){
 	char u;
 	int aux=0;
